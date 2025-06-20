de57110563b007e1b9667a605d19617fb1ff4ae1
hpc033 <33014422+hpc033@users.noreply.github.com>
2025-06-20 23:51:25 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,63 +4,100 @@ import axios from "axios";
 import * as qs from "querystring";
 
 (async () => {
-  try {
-    // 读取并校验输入参数
-    const [productId, scheduleId, seatId, webhookUrl] = [
-      "product-id",
-      "schedule-id",
-      "seat-id",
-      "slack-incoming-webhook-url",
-    ].map((name) => {
-      const value = core.getInput(name);
-      if (!value) {
-        throw new Error(`melon-ticket-actions: Please set ${name} input parameter`);
-      }
-      return value;
-    });
-
-    const message = core.getInput("message") || "买票";
-
-    // 初始化 Slack webhook
-    const webhook = new IncomingWebhook(webhookUrl);
-
-    // 请求接口，注意添加请求头，防止 406
-    const res = await axios({
-      method: "POST",
-      url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
-      params: {
-        v: "1",
-      },
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-          "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-      },
-      data: qs.stringify({
-        prodId: productId,
-        scheduleNo: scheduleId,
-        seatId,
-        volume: 1,
-        selectedGradeVolume: 1,
-      }),
-    });
-
-    console.log("Got response: ", res.data);
-
-    if (res.data.chkResult) {
-      // 生成带 langCd=EN 参数的链接
-      const link = `https://tkglobal.melon.com/performance/index.htm?${qs.stringify({
-        langCd: "EN",
-        prodId: productId,
-      })}`;
-      await webhook.send(`${message} ${link}`);
-      console.log("Slack message sent.");
-    } else {
-      console.log("Tickets not available.");
+  // Validate parameters
+  const [ productId, scheduleId, seatId, webhookUrl ] = [
+    "product-id",
+    "schedule-id",
+    "seat-id",
+    "slack-incoming-webhook-url",
+  ].map((name) => {
+    const value = core.getInput(name);
+    if (!value) {
+      throw new Error(melon-ticket-actions: Please set ${name} input parameter);
     }
-  } catch (e: any) {
-    console.error(e.stack);
-    core.setFailed(e.message);
+
+    return value;
+  });
+
+  const message = core.getInput("message") ?? "티켓사세요";
+
+  const webhook = new IncomingWebhook(webhookUrl);
+
+  const res = await axios({
+    method: "POST",
+    url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
+    params: {
+      v: "1",
+    },
+    data: qs.stringify({
+      prodId: productId,
+      scheduleNo: scheduleId,
+      seatId,
+      volume: 1,
+      selectedGradeVolume: 1,
+    }),
+  });
+
+  // tslint:disable-next-line
+  console.log("Got response: ", res.data);
+
+  if (res.data.chkResult) {
+    const link = http://ticket.melon.com/performance/index.htm?${qs.stringify({
+      prodId: productId,
+    })};
+
+    await webhook.send(${message} ${link});
   }
-})();
+})().catch((e) => {
+  console.error(e.stack); // tslint:disable-line
+  core.setFailed(e.message);
+});index.js为"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = require("@actions/core");
+const webhook_1 = require("@slack/webhook");
+const axios_1 = require("axios");
+const qs = require("querystring");
+(async () => {
+    var _a;
+    // Validate parameters
+    const [productId, scheduleId, seatId, webhookUrl] = [
+        "product-id",
+        "schedule-id",
+        "seat-id",
+        "slack-incoming-webhook-url",
+    ].map((name) => {
+        const value = core.getInput(name);
+        if (!value) {
+            throw new Error(melon-ticket-actions: Please set ${name} input parameter);
+        }
+        return value;
+    });
+    const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "买票";
+    const webhook = new webhook_1.IncomingWebhook(webhookUrl);
+    const res = await axios_1.default({
+        method: "POST",
+        url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
+        params: {
+            v: "1",
+        },
+        data: qs.stringify({
+            prodId: productId,
+            scheduleNo: scheduleId,
+            seatId,
+            volume: 1,
+            selectedGradeVolume: 1,
+        }),
+    });
+    // tslint:disable-next-line
+    console.log("Got response: ", res.data);
+    if (res.data.chkResult) {
+        const link = http://ticket.melon.com/performance/index.htm?${qs.stringify({
+            langCd: "EN",
+            prodId: productId,
+        })};
+        await webhook.send(${message} ${link});
+    }
+})().catch((e) => {
+    console.error(e.stack); // tslint:disable-line
+    core.setFailed(e.message);
+});
