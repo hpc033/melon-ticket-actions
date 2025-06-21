d62b9e080277b216ceb52804e74f20d7ffe57651
hpc033 <33014422+hpc033@users.noreply.github.com>
2025-06-21 11:22:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,30 +28,28 @@ const qs = require("querystring");
 });
     const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "买票";
     const webhook = new webhook_1.IncomingWebhook(webhookUrl);
-    const res = await axios_1.default({
-        method: "POST",
-        url: "https://tkglobal.melon.com/tktapi/product/seatStateInfo.json",
-        params: {
-            v: "1",
-        },
-        data: qs.stringify({
-            prodId: productId,
-            scheduleNo: scheduleId,
-            seatId,
-            volume: 1,
-            selectedGradeVolume: 1,
-        }),
-        headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Origin': 'https://tkglobal.melon.com',
-        'Referer': `https://tkglobal.melon.com/performance/index.htm?${qs.stringify({
-            langCd: "EN",
-            prodId: productId,
-        })}`
-    },
-    });
+   const res = await axios_1.default({
+  method: "POST",
+  url: "https://tkglobal.melon.com/tktapi/product/seatStateInfo.json",
+  params: { v: "1" },
+  data: qs.stringify({
+    prodId: productId,
+    scheduleNo: scheduleId,
+    seatId,
+    volume: 1,
+    selectedGradeVolume: 1,
+  }),
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Origin': 'https://tkglobal.melon.com',
+    'Referer': `https://tkglobal.melon.com/performance/index.htm?${qs.stringify({
+      langCd: "EN",
+      prodId: productId,
+    })}`,
+  },
+});
     // tslint:disable-next-line
     console.log("Got response: ", res.data);
     if (res.data.chkResult) {
